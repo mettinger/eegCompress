3d0339702a8c45ffdd57b2b8f405c2d4426adfe1
Mark Ettinger <jettinger35@gmail.com>
2025-01-17 16:21:24 +0000

    message

--- eegCompressModels.py
+++ eegCompressModels.py
@@ -1,4 +1,5 @@
 import torch
+from torch.utils.data.dataset import Dataset
 import numpy as np
 
 def sizeToLayerList(encoderSizeList, 
@@ -44,7 +45,7 @@ class AE(torch.nn.Module):
 		decoded = self.decoder(encoded)
 		return decoded
 
-class CustomDataset(torch.utils.data.dataset.Dataset):
+class CustomDataset(Dataset):
 	def __init__(self, eegNumpy, numSampleInput):
 		self.eegNumpy = eegNumpy
 		self.numSampleInput = numSampleInput
@@ -57,46 +58,3 @@ class CustomDataset(torch.utils.data.dataset.Dataset):
 		image = np.reshape(self.eegNumpy[:,idx:idx + self.numSampleInput], (self.nChannel * self.numSampleInput,-1), order='F').transpose().astype('float32')
 		return image, 0
 
-
-
-'''
-def sizeToLayerList(sizeList, finalStripBool):
-	layerList = []
-	for i in range(0, len(sizeList) - 1):
-		thisLayer = torch.nn.Linear(sizeList[i], sizeList[i+1])
-		#torch.nn.init.xavier_uniform_(thisLayer.weight) 
-
-		layerList.append(thisLayer)
-		layerList.append(torch.nn.ReLU())
-
-	if finalStripBool:
-		layerList.pop()
-	return layerList
-'''
-
-'''
-self.encoder = torch.nn.Sequential(
-			torch.nn.Linear(inSize, 128),
-			torch.nn.ReLU(),
-			torch.nn.Linear(128, 64),
-			torch.nn.ReLU(),
-			torch.nn.Linear(64, 36),
-			torch.nn.ReLU(),
-			torch.nn.Linear(36, 18),
-			torch.nn.ReLU(),
-			torch.nn.Linear(18, outSize)
-		)
-		
-		self.decoder = torch.nn.Sequential(
-			torch.nn.Linear(outSize, 18),
-			torch.nn.ReLU(),
-			torch.nn.Linear(18, 36),
-			torch.nn.ReLU(),
-			torch.nn.Linear(36, 64),
-			torch.nn.ReLU(),
-			torch.nn.Linear(64, 128),
-			torch.nn.ReLU(),
-			torch.nn.Linear(128, inSize),
-			torch.nn.Sigmoid()
-		)
-'''
